7bb4018ca159632a113b0c1bad16fbb0b6e40694
ShaneM123 <shanemoloneybusiness@gmail.com>
2020-11-03 19:47:03 +0000

    added json parse

--- src/events.rs
+++ src/events.rs
@@ -1,53 +1,26 @@
-use error_chain::error_chain;
 use std::io::Read;
 use reqwest::blocking::get as get_req;
+use std::collections::HashMap;
+use serde::{Serialize,Deserialize};
 
- const PREDICT_SECRET = "XhM9SVnEpHrm5PUxgaGFD6MQRbRRmjG6Hql9LH7u";
+const PREDICT_SECRET: &str = "XhM9SVnEpHrm5PUxgaGFD6MQRbRRmjG6Hql9LH7u";
 
-error_chain! {
-    foreign_links {
-        Io(std::io::Error);
-        HttpRequest(reqwest::Error);
-    }
+#[derive(DeserializeOwned, Deserialize, Debug)]
+pub struct Events {
+
 }
 
-pub fn events()-> Result<String, E>  {
-    authenticate();
+pub fn events() -> Events{
     let client = reqwest::blocking::Client::new();
-    let mut res = client.post("https://api.predicthq.com/v1/events/")
-        .bearer_auth(PREDICT_SECRET)?;
-    let mut body = String::new();
-    res.read_to_string(&mut body)?;
+    let mut res = client.get("https://api.predicthq.com/v1/events/")
+        .bearer_auth(PREDICT_SECRET)
+        .send()
+        .unwrap()
+        .json::<Events>()
+        .unwrap();
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
-    Ok(body)
+
+    return res;
 }
 
-pub fn authenticate(){
 
-    let auth = get_req("GET /v3/users/me HTTP 1.1
-Host: www.eventbriteapi.com
-Authorization: Bearer ON35GYCZ35WUAG7SPZPF");
-
-    // sync post request of some json.
-// requires feature:
-// `ureq = { version = "*", features = ["json"] }`
-    let resp = ureq::post("https://www.eventbriteapi.com/v3")
-        .set("Authorization", "ON35GYCZ35WUAG7SPZPF")
-        .send_json(serde_json::json!({
-        "name": "martin",
-        "rust": true
-    }));
-
-// .ok() tells if response is 200-299.
-    if resp.ok() {
-        println!("success: {}", resp.into_string()?);
-    } else {
-        // This can include errors like failure to parse URL or
-        // connect timeout. They are treated as synthetic
-        // HTTP-level error statuses.
-        println!("error {}: {}", resp.status(), resp.into_string()?);
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use std::thread;
 
 fn main() {
     println!("Hello, world!");
-    let weather_thread =  thread::Builder::new()
-        .name(String::from("weather thread"))
-        .spawn(weather::weather()).expect("weather thread failed to spawn");
+    // let weather_thread =  thread::Builder::new()
+    //     .name(String::from("weather thread"))
+    //     .spawn(weather::weather()).expect("weather thread failed to spawn");
 
     let events_thread = thread::Builder::new()
         .name(String::from("Event Thread"))
         .spawn(events::events()).expect("Even thread failed to spawn");
 
-    weather_thread.join().expect("failed to join weather thread");
+  //  weather_thread.join().expect("failed to join weather thread");
 
     events_thread.join().expect("failed to join event thread");
 
